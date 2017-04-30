c193d17237f60c0c30b560a5d2d691d5c6cf83c9
Michael Tesař <michael.tesar@nudz.cz>
2017-04-30 21:22:24 +0200

    Clean repo

--- ledlights.m
+++
@@ -1,5 +0,0 @@
-function vectorData = ledlights(colorStreamMatrix)
-    dataSize = size(colorStreamMatrix);
-    dataSize = dataSize(1) * dataSize(2);
-    vectorData = reshape(colorStreamMatrix', [1, dataSize]);
-end
